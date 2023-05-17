26219c0483f4d5bbbfc2a06d4229351742542fbe
Vandopal <79451947+Vandopal@users.noreply.github.com>
2023-05-17 14:08:06 +0300

    brain-games.js correct

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 export default function greeting() {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
+  return console.log(`Hello, ${name}`);
 }
 greeting();
